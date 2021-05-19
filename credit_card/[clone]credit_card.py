07f09c6b5a2e939eba2f25a29dd2b1ab62a1e771
HC-kang <weston0713@gmail.com>
2021-05-19 22:25:44 +0900

    0519 commit

---
+++ credit_card/[clone]credit_card.py
@@ -0,0 +1,23 @@
+import os
+os.chdir('/Users/heechankang/projects/pythonworkspace/dacon_data/credit_card')
+
+import pandas as pd
+import warnings
+warnings.filterwarnings('ignore')
+
+train = pd.read_csv('funda_train.csv')
+submission = pd.read_csv('submission.csv')
+
+train.shape
+train.head()
+train.info( )
+train.isnull().sum()
+train.isnull()
+train.describe()
+
+submission.shape
+submission.head()
+submission.info()
+submission.describe()
+
+train[train['amount']<0]
